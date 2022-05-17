da884b6e8409a081c5980ed96e35e016bed7a615
varunkumarkadambala <varunkmr26@gmail.com>
2022-05-17 17:20:28 +0100

    Split Code to Modules

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,45 +4,51 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import java.io.*;
-import java.lang.reflect.Array;
 import java.net.*;
 import java.util.*;
-import java.util.function.Function;
-//
-//public class Comparator<Map<String, Object>> mapComparator = new Comparator<Map<String, Object>>() {
-//    public int compare(Map<String, String> m1, Map<String, String> m2) {
-//        return m1.get("name").compareTo(m2.get("name"));
-//    }
-//}
+
 
 public class Main {
+    static final int connectTimeout = 5000;
+    static final int readTimeout = 5000;
+    static final String newsServiceURL = "http://localhost:8080/api/news";
 
-    public static void main(String[] args) throws IOException, ParseException {
-	    // Read the request
-        URL url = new URL("http://localhost:8080/api/news");
+    // Call News Service URL
+    public static String callNewsService(String newsServiceURL) throws IOException {
+        URL url = new URL(newsServiceURL);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("Content-Type", "application/json");
-        con.setConnectTimeout(5000);
-        con.setReadTimeout(5000);
+        con.setConnectTimeout(connectTimeout);
+        con.setReadTimeout(readTimeout);
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
+
         String inputLine;
         StringBuffer response = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
         }
         in.close();
+
         con.disconnect();
 
-        // Convert Response to JSONArray
-        JSONParser parser = new JSONParser();
-        JSONArray json = (JSONArray) parser.parse(String.valueOf(response));
-//        System.out.print(json);
+        return String.valueOf(response);
+    }
 
-        // Iterating through the json response
+    // Parse JSON String to JSON Object
+    public static JSONArray parseJson(String jsonString) throws ParseException {
+
+        JSONParser parser = new JSONParser();
+        JSONArray json = (JSONArray) parser.parse(jsonString);
+
+        return json;
+    }
+
+    // Generate News Reports
+    public static List<Map<String, String>> generateNewsReport(JSONArray json){
+
         List<Map<String, String>> reportList = new ArrayList<>();
         Iterator i = json.iterator();
 
@@ -54,8 +60,8 @@ public class Main {
             JSONArray tags = (JSONArray) article.get("tags");
             Long weight = (Long) article.get("weight");
 
-            // Ignore if Category is Social
-            if(category!="SOCIAL"){
+            // Ignore if Category is Social and Add the data to a list of Maps
+            if(!category.equals("SOCIAL")){
                 Map<String, String> mapObject = new HashMap<>();
                 mapObject.put("id", id.substring(id.length()-10));
                 mapObject.put("category", category);
@@ -66,21 +72,15 @@ public class Main {
             }
         }
 
-        // Sort the Report list
+        // Sort the Report list based on Weights
         Comparator<Map<String, String>> sortByWeight = Comparator.comparing(x -> -1 * Integer.valueOf(x.get("weight")));
         reportList.sort(sortByWeight);
 
-//        for(Map<String, String> mapObject : reportList)
-//        {
-//            System.out.println(mapObject.get("id"));
-//            System.out.println(mapObject.get("category"));
-//            System.out.println(mapObject.get("title"));
-//            System.out.println(mapObject.get("tags"));
-//            System.out.println(mapObject.get("weight"));
-//            System.out.println();
-//        }
+        return reportList;
+    }
 
-        // Write List to File
+    // Generate File Output
+    public static void generateFileOutput(List<Map<String, String>> reportList) throws IOException {
         FileWriter myWriter = new FileWriter("edited_articles.txt");
         for(Map<String, String> mapObject : reportList)
         {
@@ -103,4 +103,14 @@ public class Main {
 
         myWriter.close();
     }
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+        String response = callNewsService(newsServiceURL);
+        JSONArray newsList = parseJson(response);
+        List<Map<String, String>> newsReportList = generateNewsReport(newsList);
+        generateFileOutput(newsReportList);
+        return;
+    }
+
 }
